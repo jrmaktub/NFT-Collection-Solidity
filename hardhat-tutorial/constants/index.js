dd47bb6c2e6794f90635def2561cb25d8ff9103c
jramirez09 <jramirez12@luc.edu>
2023-03-09 15:40:34 -0600

    fixed contract

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x83404E20C7e2e86c92d36784266096acE43daBd8";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "nft-collection-solidity-nine.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
